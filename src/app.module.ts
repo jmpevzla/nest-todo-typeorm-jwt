c06990ceb96d3e441229ef888bead1df978f854d
Jose M Perez <jmperezvzla@gmail.com>
2022-01-23 00:50:27 -0400

    Add auth & user code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as typeOrmConfig from './typeorm.config';
 
 @Module({
-  imports: [AuthModule, UserModule, TodoModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule,
+    UserModule,
+    TodoModule,
+  ],
   controllers: [],
   providers: [],
 })
