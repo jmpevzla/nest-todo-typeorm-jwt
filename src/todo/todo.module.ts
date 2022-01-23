fb943bb2d133375eef3bd3c529785c527bf68703
Jose M Perez <jmperezvzla@gmail.com>
2022-01-23 01:27:20 -0400

    Add Todo Module

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoRepository } from './repository/todo.repository';
+import { TodoService } from './todo.service';
+import { TodoController } from './todo.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([TodoRepository])],
+  controllers: [TodoController],
+  providers: [TodoService],
+})
 export class TodoModule {}
